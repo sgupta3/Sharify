1d0eb503ab5832eb2d0b69eb829d339f69837eb3
Sahil Gupta <emailsahilgupta12@gmail.com>
2015-05-28 21:50:15 -0400

    Setup storyboard.

---
+++ Sharify/InboxViewController.h
@@ -0,0 +1,13 @@
+//
+//  InboxViewController.h
+//  Sharify
+//
+//  Created by Sahil Gupta on 2015-05-28.
+//  Copyright (c) 2015 SahilGupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InboxViewController : UITableViewController
+
+@end
