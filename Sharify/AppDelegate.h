2d881c6929d29a2b35dd342ee2e3238e7abd536c
Sahil Gupta <emailsahilgupta12@gmail.com>
2015-05-28 21:22:31 -0400

    Initial Commit

---
+++ Sharify/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sharify
+//
+//  Created by Sahil Gupta on 2015-05-28.
+//  Copyright (c) 2015 SahilGupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
