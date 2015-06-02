6910126a3f73f7ad55696e9bd05b48c0500d3b3a
Sahil Gupta <emailsahilgupta12@gmail.com>
2015-06-02 18:55:37 -0400

    User see the list of users and add them as friends.

--- Sharify/EditFriendsTableViewController.m
+++ Sharify/EditFriendsTableViewController.m
@@ -7,7 +7,6 @@
 //
 
 #import "EditFriendsTableViewController.h"
-#import <Parse/Parse.h>
 
 @interface EditFriendsTableViewController ()
 
@@ -16,31 +15,61 @@
 @implementation EditFriendsTableViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];    
+    [super viewDidLoad];
+    PFQuery *query = [PFUser query];
+    [query orderByAscending:@"username"];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error){
+        if(error) {
+            NSLog(@"Error: %@ %@",error,[error userInfo]);
+        } else {
+            self.allUsers = objects;
+            [self.tableView reloadData];
+        }
+    }];
+    
+    self.currentUser = [PFUser currentUser];
 
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    // Return the number of sections.
     return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
+    return self.allUsers.count;
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    PFUser *user = [self.allUsers objectAtIndex:indexPath.row];
+    cell.textLabel.text = user.username;
+    
+    
+    return cell;
 }
-*/
+
+-(void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    
+    PFRelation *friendsRelation = [self.currentUser relationForKey:@"friendsRelation"];
+    PFUser *user = [self.allUsers objectAtIndex:indexPath.row];
+    [friendsRelation addObject:user];
+    
+    [self.currentUser saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if(error) {
+            NSLog(@"Error %@ %@", error, [error userInfo]);
+        } else {
+            
+        }
+    }];
+}
 
 @end
